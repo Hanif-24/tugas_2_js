4ca808b363b66472c5ad66c6c5f2441fa048d0e4
Hanif-Niomic <m.alihanif249@gmail.com>
2021-12-18 19:55:54 +0700

    Tugas 2 Js
    Tugas 2 Js

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number = -20.5;
+var string = 'Selasa, 14 Desember 2021';
+var boolean = true;
+var ud;
+var array = ['Senin','Selasa','Rabu','Kamis','Jumat'];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(ud);
+console.log(array[3]);
